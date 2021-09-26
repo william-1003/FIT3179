25eb7d3da99065810a19ba6946e3fa9612de586d
william-1003 <55171192+william-1003@users.noreply.github.com>
2021-09-26 14:24:27 +1000

    Add files via upload

---
+++ Week 9 Homework/js/vega_lite_vis.js
@@ -0,0 +1,5 @@
+var vg_1 = "energy_consumption_per_capita.json";
+
+vegaEmbed("#chloropleth_map", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
